b7515dbf4bb5945ba6042e014ea5557435cd11bd
eiliskeeble <eiliskeeble@msn.com>
2021-10-27 16:00:43 +0000

    Trial adding new variables

--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -7,8 +7,26 @@ study = StudyDefinition(
         "rate": "uniform",
         "incidence": 0.5,
     },
+
     population=patients.registered_with_one_practice_between(
-        "2019-02-01", "2020-02-01"
+        "2018-03-01", "2020-02-28"
+    ),
+
+    death_date=patients.died_from_any_cause(
+        between=["2019-03-01", "2021-02-28"],
+        returning="date_of_death",
+        date_format="YYYY-MM-DD",
+        return_expectations={
+            "date": {"earliest": "2019-03-01"},
+            "rate": "exponential_increase"
+        },
+    ),
+
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        },
     ),
 
     age=patients.age_as_of(
@@ -18,4 +36,14 @@ study = StudyDefinition(
             "int": {"distribution": "population_ages"},
         },
     ),
+
+    admission=patients.admitted_to_hospital(
+        returning="binary_flag",
+        on_or_before="death_date",
+        find_first_match_in_period=True,
+        date_format="YYYY-MM-DD",
+        return_expectations={"date": {"earliest": "2018-03-01"}},
+    ),
 )
+
+# How to refer to created variables when creating new?
