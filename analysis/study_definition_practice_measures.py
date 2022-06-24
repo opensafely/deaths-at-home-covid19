7bdee0c27d16c6105d13eebed305c479c676e873
Eilís <72603602+eiliskeeble@users.noreply.github.com>
2022-06-24 09:59:15 +0100

    Add eol medications to practice measures
    Also add palliative care which was missed

--- analysis/study_definition_practice_measures.py
+++ analysis/study_definition_practice_measures.py
@@ -134,13 +134,13 @@ study = StudyDefinition(
             }
     ),
 
-    beddays_emadm_binary = patients.admitted_to_hospital(
+    embeddays_binary = patients.admitted_to_hospital(
         returning = "binary_flag",
         between = ["index_date", "last_day_of_month(index_date)"],
         with_admission_method = ['21', '2A', '22', '23', '24', '25', '2D']
     ),
 
-    beddays_emadm = patients.admitted_to_hospital(
+    embeddays = patients.admitted_to_hospital(
         returning = "total_bed_days_in_period",
         between = ["index_date", "last_day_of_month(index_date)"],
         with_admission_method = ['21', '2A', '22', '23', '24', '25', '2D'],
@@ -150,13 +150,13 @@ study = StudyDefinition(
             }
     ),
 
-    beddays_eladm_binary = patients.admitted_to_hospital(
+    elbeddays_binary = patients.admitted_to_hospital(
         returning = "binary_flag",
         between = ["index_date", "last_day_of_month(index_date)"],
         with_admission_method = ['11', '12', '13']
     ),
 
-    beddays_eladm = patients.admitted_to_hospital(
+    elbeddays = patients.admitted_to_hospital(
         returning = "total_bed_days_in_period",
         between = ["index_date", "last_day_of_month(index_date)"],
         with_admission_method = ['11', '12', '13'],
@@ -180,13 +180,13 @@ study = StudyDefinition(
             }
     ),
 
-    crit_beddays_emadm_binary = patients.admitted_to_hospital(
+    crit_embeddays_binary = patients.admitted_to_hospital(
         returning = "binary_flag",
         between = ["index_date", "last_day_of_month(index_date)"],
         with_admission_method = ['21', '2A', '22', '23', '24', '25', '2D']
     ),
 
-    crit_beddays_emadm = patients.admitted_to_hospital(
+    crit_embeddays = patients.admitted_to_hospital(
         returning = "total_critical_care_days_in_period",
         between = ["index_date", "last_day_of_month(index_date)"],
         with_admission_method = ['21', '2A', '22', '23', '24', '25', '2D'],
@@ -196,13 +196,13 @@ study = StudyDefinition(
             }
     ),
 
-    crit_beddays_eladm_binary = patients.admitted_to_hospital(
+    crit_elbeddays_binary = patients.admitted_to_hospital(
         returning = "binary_flag",
         between = ["index_date", "last_day_of_month(index_date)"],
         with_admission_method = ['11', '12', '13']
     ),
 
-    crit_beddays_eladm = patients.admitted_to_hospital(
+    crit_elbeddays = patients.admitted_to_hospital(
         returning = "total_critical_care_days_in_period",
         between = ["index_date", "last_day_of_month(index_date)"],
         with_admission_method = ['11', '12', '13'],
@@ -261,13 +261,208 @@ study = StudyDefinition(
     ## EOL medication
     # Start with midazolam as existing codelist
     eol_med_binary = patients.with_these_medications(
-        midazolam_codes,
+        eol_med_codes,
         returning = "binary_flag",
         between = ["index_date", "last_day_of_month(index_date)"]
     ),
 
     eol_med = patients.with_these_medications(
+        eol_med_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    #Codelist with additional meds beyond priority list
+    eol_med_full_binary = patients.with_these_medications(
+        eol_med_full_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+
+    eol_med_full = patients.with_these_medications(
+        eol_med_full_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    midazolam_binary = patients.with_these_medications(
         midazolam_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    midazolam = patients.with_these_medications(
+        midazolam_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    glycopyrronium_binary = patients.with_these_medications(
+        glycopyrronium_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    glycopyrronium = patients.with_these_medications(
+        glycopyrronium_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    haloperidol_binary = patients.with_these_medications(
+        haloperidol_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    haloperidol = patients.with_these_medications(
+        haloperidol_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    hyoscine_butyl_binary = patients.with_these_medications(
+        hyoscine_butylbromide_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    hyoscine_butyl = patients.with_these_medications(
+        hyoscine_butylbromide_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    levomepromazine_binary = patients.with_these_medications(
+        levomepromazine_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    levomepromazine = patients.with_these_medications(
+        levomepromazine_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    morphine_binary = patients.with_these_medications(
+        morphine_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    morphine = patients.with_these_medications(
+        morphine_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    oxycodone_binary = patients.with_these_medications(
+        oxycodone_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    oxycodone = patients.with_these_medications(
+        oxycodone_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    hyoscine_hydro_binary = patients.with_these_medications(
+        hyoscine_hydrobromide_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    hyoscine_hydro = patients.with_these_medications(
+        hyoscine_hydrobromide_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    alfentanil_binary = patients.with_these_medications(
+        alfentanil_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    alfentanil = patients.with_these_medications(
+        alfentanil_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    diamorphine_binary = patients.with_these_medications(
+        diamorphine_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+    
+    diamorphine = patients.with_these_medications(
+        diamorphine_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    ## Palliative care
+    palliative_binary = patients.with_these_clinical_events(
+        palcare_codes1,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+
+
+    palliative = patients.with_these_clinical_events(
+        palcare_codes1,
         returning = "number_of_matches_in_period",
         between = ["index_date", "last_day_of_month(index_date)"],
         return_expectations = {
@@ -378,8 +573,41 @@ study = StudyDefinition(
             "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
             "incidence": 0.8
             }
-    )
+    ),
 
+    ## All MDT
+    all_mdt_binary = patients.with_these_clinical_events(
+        all_mdt_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+
+    all_mdt = patients.with_these_clinical_events(
+        all_mdt_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    ),
+
+    ## Community
+    community_binary = patients.with_these_clinical_events(
+        community_codes,
+        returning = "binary_flag",
+        between = ["index_date", "last_day_of_month(index_date)"]
+    ),
+
+    community = patients.with_these_clinical_events(
+        community_codes,
+        returning = "number_of_matches_in_period",
+        between = ["index_date", "last_day_of_month(index_date)"],
+        return_expectations = {
+            "int": {"distribution": "normal", "mean": 5, "stddev": 1}, 
+            "incidence": 0.8
+            }
+    )
 )
 
 measures = [
@@ -419,15 +647,15 @@ measures = [
     ),
 
     Measure(
-        id="beddays_emadm_by_practice_binary",
-        numerator="beddays_emadm_binary",
+        id="embeddays_by_practice_binary",
+        numerator="embeddays_binary",
         denominator="population",
         group_by="practice_id",
     ),
 
     Measure(
-        id="beddays_eladm_by_practice_binary",
-        numerator="beddays_eladm_binary",
+        id="elbeddays_by_practice_binary",
+        numerator="elbeddays_binary",
         denominator="population",
         group_by="practice_id",
     ),
@@ -440,15 +668,15 @@ measures = [
     ),
 
     Measure(
-        id="crit_beddays_emadm_by_practice_binary",
-        numerator="crit_beddays_emadm_binary",
+        id="crit_embeddays_by_practice_binary",
+        numerator="crit_embeddays_binary",
         denominator="population",
         group_by="practice_id",
     ),
 
     Measure(
-        id="crit_beddays_eladm_by_practice_binary",
-        numerator="crit_beddays_eladm_binary",
+        id="crit_elbeddays_by_practice_binary",
+        numerator="crit_elbeddays_binary",
         denominator="population",
         group_by="practice_id",
     ),
@@ -477,6 +705,90 @@ measures = [
     Measure(
         id="eol_med_by_practice_binary",
         numerator="eol_med_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="eol_med_full_by_practice_binary",
+        numerator="eol_med_full_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="midazolam_by_practice_binary",
+        numerator="midazolam_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="glycopyrronium_by_practice_binary",
+        numerator="glycopyrronium_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="haloperidol_by_practice_binary",
+        numerator="haloperidol_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="hyoscine_butly_by_practice_binary",
+        numerator="hyoscine_butyl_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="levomepromazine_by_practice_binary",
+        numerator="levomepromazine_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="morphine_by_practice_binary",
+        numerator="morphine_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="oxycodone_by_practice_binary",
+        numerator="oxycodone_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="hyoscine_hydro_by_practice_binary",
+        numerator="hyoscine_hydro_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="alfentanil_by_practice_binary",
+        numerator="alfentanil_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="diamorphine_by_practice_binary",
+        numerator="diamorphine_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="palliative_by_practice_binary",
+        numerator="palliative_binary",
         denominator="population",
         group_by="practice_id",
     ),
@@ -524,6 +836,20 @@ measures = [
     ),
 
     Measure(
+        id="all_mdt_by_practice_binary",
+        numerator="all_mdt_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="community_by_practice_binary",
+        numerator="community_binary",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
         id="aevis_by_practice_rate",
         numerator="aevis",
         denominator="population",
@@ -559,15 +885,15 @@ measures = [
     ),
 
     Measure(
-        id="beddays_emadm_by_practice_rate",
-        numerator="beddays_emadm",
+        id="embeddays_by_practice_rate",
+        numerator="embeddays",
         denominator="population",
         group_by="practice_id",
     ),
 
     Measure(
-        id="beddays_eladm_by_practice_rate",
-        numerator="beddays_eladm",
+        id="elbeddays_by_practice_rate",
+        numerator="elbeddays",
         denominator="population",
         group_by="practice_id",
     ),
@@ -580,15 +906,15 @@ measures = [
     ),
 
     Measure(
-        id="crit_beddays_emadm_by_practice_rate",
-        numerator="crit_beddays_emadm",
+        id="crit_embeddays_by_practice_rate",
+        numerator="crit_embeddays",
         denominator="population",
         group_by="practice_id",
     ),
 
     Measure(
-        id="crit_beddays_eladm_by_practice_rate",
-        numerator="crit_beddays_eladm",
+        id="crit_elbeddays_by_practice_rate",
+        numerator="crit_elbeddays",
         denominator="population",
         group_by="practice_id",
     ),
@@ -617,6 +943,90 @@ measures = [
     Measure(
         id="eol_med_by_practice_rate",
         numerator="eol_med",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="eol_med_full_by_practice_rate",
+        numerator="eol_med_full",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="midazolam_by_practice_rate",
+        numerator="midazolam",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="glycopyrronium_by_practice_rate",
+        numerator="glycopyrronium",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="haloperidol_by_practice_rate",
+        numerator="haloperidol",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="hyoscine_butly_by_practice_rate",
+        numerator="hyoscine_butyl",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="levomepromazine_by_practice_rate",
+        numerator="levomepromazine",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="morphine_by_practice_rate",
+        numerator="morphine",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="oxycodone_by_practice_rate",
+        numerator="oxycodone",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="hyoscine_hydro_by_practice_rate",
+        numerator="hyoscine_hydro",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="alfentanil_by_practice_rate",
+        numerator="alfentanil",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="diamorphine_by_practice_rate",
+        numerator="diamorphine",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="palliative_by_practice_rate",
+        numerator="palliative",
         denominator="population",
         group_by="practice_id",
     ),
@@ -659,6 +1069,20 @@ measures = [
     Measure(
         id="cancer_mdt_by_practice_rate",
         numerator="cancer_mdt",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="all_mdt_by_practice_rate",
+        numerator="all_mdt",
+        denominator="population",
+        group_by="practice_id",
+    ),
+
+    Measure(
+        id="community_by_practice_rate",
+        numerator="community",
         denominator="population",
         group_by="practice_id",
     )
